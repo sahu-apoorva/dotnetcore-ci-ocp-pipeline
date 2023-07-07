4c360dfb5202269ec17e15ddb780c61ef91cf9dc
SaifRehman <s4saif.121@gmail.com>
2023-07-07 15:04:06 +0100

    new change

--- SampleApplication/Controllers/WeatherForecastController.cs
+++ SampleApplication/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "saif changes", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "new change", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
